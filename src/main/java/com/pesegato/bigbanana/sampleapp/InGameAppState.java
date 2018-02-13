f3553eb564c2b79b6ce3d4e6410d63e6c949c6c1
Pesegato <pesegato@outlook.com>
2018-02-13 11:08:53 +0100

    Sample app goes ingame

--- src/main/java/com/pesegato/bigbanana/sampleapp/InGameAppState.java
+++ src/main/java/com/pesegato/bigbanana/sampleapp/InGameAppState.java
@@ -1,11 +1,33 @@
 package com.pesegato.bigbanana.sampleapp;
 
 import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.BaseAppState;
+import com.jme3.scene.Node;
+import com.pesegato.bigbanana.BBFocusTraversal;
+import com.simsilica.lemur.GuiGlobals;
 
-public class InGameAppState extends BaseAppState{
+public class InGameAppState extends BaseAppState {
     @Override
     protected void initialize(Application app) {
+        Node gui = ((SimpleApplication) getApplication()).getGuiNode();
+        IngameBanana b[][] = new IngameBanana[3][3];
+        BBFocusTraversal bbft = new BBFocusTraversal();
+        gui.addControl(bbft);
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                b[i][j] = new IngameBanana(app.getAssetManager(),50,50);
+                b[i][j].geo.setLocalTranslation(100 + i * 100, 400 - j * 100, 0);
+                gui.attachChild(b[i][j].geo);
+                bbft.addFocusable(b[i][j].geo, j, i, 1, 1);
+            }
+        }
+        IngameBanana bottom=new IngameBanana(app.getAssetManager(),250,50);
+        bottom.geo.setLocalTranslation(100,100,0);
+        gui.attachChild(bottom.geo);
+        bbft.addFocusable(bottom.geo,3,0,3,1);
+
+        GuiGlobals.getInstance().requestFocus(b[0][0].geo);
 
     }
 
---
+++ src/main/java/com/pesegato/bigbanana/sampleapp/IngameBanana.java
@@ -0,0 +1,59 @@
+package com.pesegato.bigbanana.sampleapp;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Quad;
+import com.pesegato.bigbanana.BBFocusTarget;
+import com.simsilica.lemur.input.FunctionId;
+import com.simsilica.lemur.input.InputState;
+
+public class IngameBanana {
+    public Geometry geo;
+
+    public IngameBanana(AssetManager assetManager, int width, int height) {
+        geo = new Geometry("MyQuad", new Quad(width, height));
+        Material m = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        m.setColor("Color", ColorRGBA.Yellow);
+        geo.setMaterial(m);
+        geo.addControl(new AAFocusTarget());
+    }
+
+    class AAFocusTarget extends BBFocusTarget {
+
+        @Override
+        public void onFocusGain() {
+            geo.getMaterial().setColor("Color", ColorRGBA.Blue);
+        }
+
+        @Override
+        public void onFocusLost() {
+            geo.getMaterial().setColor("Color", ColorRGBA.Yellow);
+        }
+
+        @Override
+        public boolean isFocusable() {
+            return true;
+        }
+
+        @Override
+        public void valueChanged(FunctionId func, InputState value, double tpf) {
+            /*
+            if( pressed && value == InputState.Off ) {
+                // Do click processing... the mouse does click processing before
+                // up processing so we will too
+                runClick();
+            }
+            // Only mapped to one function so no need to distinguish
+setPressed(isEnabled() && value == InputState.Positive);
+             */
+            if (value == InputState.Positive) {
+                geo.getMaterial().setColor("Color", ColorRGBA.Red);
+            } else {
+                geo.getMaterial().setColor("Color", ColorRGBA.Blue);
+            }
+        }
+    }
+
+}
